9d5969f34cce442cbecae2c569868b1f7ff859fc
王开琦 <wkq97@qq.com>
2023-09-16 09:56:56 +0800

    open

--- middleware/openResty1/lua/resume.lua
+++ middleware/openResty1/lua/resume.lua
@@ -1,3 +1,5 @@
 --ngx.say('{"status":5711,"msg":"这是通过openresty自定义！","success":false,"data":null}')
  -- http 协议版本
-ngx.say("HTTP协议版本: ", ngx.req.http_version());
+--ngx.say("HTTP协议版本: ", ngx.req.http_version());
+-- 请求方法
+ngx.say("请求方法: ", ngx.req.get_method());
