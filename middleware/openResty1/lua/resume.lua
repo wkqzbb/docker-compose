06186662b6426a7a530548e38b9a50100b8983ce
王开琦 <wkq97@qq.com>
2023-09-16 10:09:24 +0800

    open

--- middleware/openResty1/lua/resume.lua
+++ middleware/openResty1/lua/resume.lua
@@ -4,4 +4,8 @@
 -- 请求方法
 --ngx.say("请求方法: ", ngx.req.get_method());
 -- 获取原始请求头信息
-ngx.say("原始请求头信息: " .. ngx.req.raw_header());
+--ngx.say("原始请求头信息: " .. ngx.req.raw_header());
+-- 获取请求头信息
+local myHeader = ngx.req.get_headers();
+ngx.say("token" .. myHeader["token"]);
+ngx.say("uid" .. myHeader["uid"]);
